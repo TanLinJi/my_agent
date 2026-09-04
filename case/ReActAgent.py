1c48fdc922cc506b017a44ea5d3834037e3953de
TanLinJi <tanlinji0311@gmail.com>
2026-09-04 16:54:06 +0800

    feat: ReActAgent demo

--- case/ReActAgent.py
+++ case/ReActAgent.py
@@ -1,4 +1,7 @@
+import re
 from llm_client import HelloAgentsLLM
+from tools import ToolExecutor, search
+
 
 # ReAct 提示词模板
 REACT_PROMPT_TEMPLATE = """
@@ -21,5 +24,113 @@ History: {history}
 """
 
 class ReActAgent:
-    def __init__(self, llm_agent: HelloAgentsLLM):
-        pass
+    def __init__(self, llm_client: HelloAgentsLLM, tool_executor: ToolExecutor, max_steps: int=5):
+        self.llm_client = llm_client
+        self.tool_executor = tool_executor
+        self.max_steps = max_steps
+        self.history = []
+
+    # run 方法是智能体的入口, 它的 while 循环构成了 ReAct 范式的主体;
+    # max_steps 参数则是一个重要的安全阀，防止智能体陷入无限循环而耗尽资源。
+    def run(self, question: str):
+        """
+        运行ReAct智能体来回答一个问题。
+        """
+        self.history = []  # 每次运行时重置历史记录
+        current_step = 0
+        idx = True
+        while current_step < self.max_steps:
+            current_step += 1
+            print(f"--- 第 {current_step} 步 ---")
+
+            # 1. 格式化提示词
+            tools_desc = self.tool_executor.getAvailableTools()
+            history_str = "\n".join(self.history)
+            prompt = REACT_PROMPT_TEMPLATE.format(
+                tools = tools_desc,
+                question = question,
+                history = history_str
+            )
+
+            if(idx):
+                print("=====提示词 begin =====")
+                print(prompt)
+                print("=====提示词  end  =====")
+                idx = False
+
+            # 2. 调用LLM进行思考
+            messages = [
+                {
+                    "role": "user",
+                    "content": prompt
+                }
+            ]
+            response_text = self.llm_client.think(messages = messages)
+
+            if not response_text:
+                print("错误:LLM未能返回有效响应。")
+                break
+
+            thought, action = self._parse_output(response_text)
+            if thought: 
+                print(f"🤔 思考: {thought}")
+            if not action: 
+                print("警告：未能解析出有效的Action，流程终止。"); 
+                break
+
+            if action.startswith("Finish"):
+                # 如果是Finish指令，提取最终答案并结束
+                final_answer = self._parse_action_input(action)
+                print(f"🎉 最终答案: {final_answer}")
+                return final_answer\
+
+            tool_name, tool_input = self._parse_action(action)
+            if not tool_name or not tool_input:
+                self.history.append("Observation: 无效的Action格式，请检查。"); 
+                continue
+
+            print(f"🎬 行动: {tool_name}[{tool_input}]")
+            tool_function = self.tool_executor.getTool(tool_name)
+            observation = tool_function(tool_input) if tool_function else f"错误：未找到名为 '{tool_name}' 的工具。"
+
+            print(f"👀 观察: {observation}")
+            self.history.append(f"Action: {action}")
+            self.history.append(f"Observation: {observation}")
+
+        print("已达到最大步数，流程终止。")
+        return None
+    
+    def _parse_output(self, text: str):
+        """
+        解析LLM的输出，提取Thought和Action。
+        """
+        # Thought: 匹配到 Action: 或文本末尾
+        thought_match = re.search(r"Thought:\s*(.*?)(?=\nAction:|$)", text, re.DOTALL)
+        # Action: 匹配到文本末尾
+        action_match = re.search(r"Action:\s*(.*?)$", text, re.DOTALL)
+        thought = thought_match.group(1).strip() if thought_match else None
+        action = action_match.group(1).strip() if action_match else None
+        return thought,action
+    
+    def _parse_action(self, action_text: str):
+        """
+        解析Action字符串，提取工具名称和输入。
+        例如从 Search[华为最新手机] 中提取出工具名 `Search` 和工具输入 `华为最新手机`。
+        """
+        match = re.match(r"(\w+)\[(.*)\]", action_text, re.DOTALL)
+        if match:
+            return match.group(1), match.group(2)
+        return None, None
+
+    def _parse_action_input(self, action_text: str):
+        match = re.match(r"\w+\[(.*)\]", action_text, re.DOTALL)
+        return match.group(1) if match else ""
+
+if __name__ == '__main__':
+    llm = HelloAgentsLLM()
+    tool_executor = ToolExecutor()
+    search_desc = "一个网页搜索引擎。当你需要回答关于时事、事实以及在你的知识库中找不到的信息时，应使用此工具。"
+    tool_executor.registerTool("Search", search_desc, search)
+    agent = ReActAgent(llm_client=llm, tool_executor=tool_executor)
+    question = "华为最新的手机是哪一款？它的主要卖点是什么？"
+    agent.run(question)
